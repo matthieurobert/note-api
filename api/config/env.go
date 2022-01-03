90319badaff7fe0bfdd7a6249955352c73a6839a
matthieurobert <matthieurobert82@gmail.com>
2022-01-03 21:29:02 +0100

    feat: Create database connection with postgres

--- api/config/env.go
+++ api/config/env.go
@@ -6,9 +6,19 @@ import (
 )
 
 type Env struct {
-	ApiPort int
+	PostgresHost     string
+	PostgresPort     int
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDatabase string
+	ApiPort          int
 }
 
 func (env *Env) Initenv() {
+	env.PostgresHost = os.Getenv("POSTGRES_HOST")
+	env.PostgresUser = os.Getenv("POSTGRES_USER")
+	env.PostgresPort, _ = strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	env.PostgresPassword = os.Getenv("POSTGRES_PASSWORD")
+	env.PostgresDatabase = os.Getenv("POSTGRES_DB")
 	env.ApiPort, _ = strconv.Atoi(os.Getenv("API_PORT"))
 }
