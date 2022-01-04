990acd1c7cafba103f7c49e86a8b373849f84269
matthieurobert <matthieurobert82@gmail.com>
2022-01-04 21:23:43 +0100

    feat: Add userId in note model and handlers

--- api/handler/postNote.go
+++ api/handler/postNote.go
@@ -6,11 +6,15 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/matthieurobert/amp/api/auth"
 	"github.com/matthieurobert/amp/api/entity"
 )
 
 func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
+
+	_, userInfo, _ := auth.Strategy.AuthenticateRequest(r)
+	user, _ := entity.GetUserByUsername(userInfo.GetUserName())
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -23,6 +27,8 @@ func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	note.UserId = user.Id
 
 	if note.Body == "" {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
